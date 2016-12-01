905fbe9066493073603d0d2a00d55a39d456e512
Chris Granade <cgranade@cgranade.com>
2016-12-01 20:43:29 +1100

    PS: Added which fn, aliases to hide native cmds.

--- .config/powershell/Microsoft.PowerShell_profile.ps1
+++ .config/powershell/Microsoft.PowerShell_profile.ps1
@@ -4,3 +4,45 @@
 function config {
 	/usr/bin/git --git-dir=$home/.cfg --work-tree=$home $args
 }
+
+# Configure aliases to point to built-in cmdlets over native commands.
+# We use -ErrorAction Ignore to deal with the case where these
+# aliases may already be defined.
+New-Alias -Name "ls" -Value Get-ChildItem -ErrorAction Ignore
+New-Alias -Name "mv" -Value Move-Item -ErrorAction Ignore
+New-Alias -Name "rm" -Value Remove-Item -ErrorAction Ignore
+New-Alias -Name "rmdir" -Value Remove-Item -ErrorAction Ignore
+
+# which is a bit more complicated...
+# In particular, Get-Command is very powerful, but
+# we often want something quick, as the less powerful
+# native command `which` gives us.
+function which {
+    param(
+        [string] $name
+    )
+
+    $cmd = Get-Command $name -ErrorAction Ignore
+    if (-not $cmd) {
+        return
+    } else {
+        switch ($cmd.CommandType) {
+            "Alias" {
+                return "{0} -> {1}" -f $cmd.Name, $cmd.Definition
+            }
+
+            "Application" {
+                return $cmd.Path
+            }
+
+            "Function" {
+                return "function {0}" -f $cmd.Name
+            }
+
+            default {
+                return $cmd
+            }
+
+        }
+    }
+}

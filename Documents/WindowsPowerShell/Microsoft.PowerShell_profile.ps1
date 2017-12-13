d9d12b8294c007ae4cea3f7beaa31893fadc41da
Christopher Granade <chgranad@microsoft.com>
2017-12-13 09:14:42 -0800

    Added PowerShell profile.

---
+++ Documents/WindowsPowerShell/Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,46 @@
+﻿# Ensure that Get-ChildItemColor is loaded
+Import-Module Get-ChildItemColor
+
+# Set l and ls alias to use the new Get-ChildItemColor cmdlets
+Set-Alias l Get-ChildItemColor -Option AllScope
+Set-Alias ls Get-ChildItemColorFormatWide -Option AllScope
+
+Import-Module posh-git
+Start-SshAgent -Quiet
+
+Add-PSSnapIn Microsoft.HPC
+
+function Remove-Bom() {
+    # See https://stackoverflow.com/a/5596984.
+    param([string] $Path);
+    $resolved = Resolve-Path $Path;
+    $contents = Get-Content $resolved;
+    $encoding = New-Object System.Text.UTF8Encoding $False;
+    [System.IO.File]::WriteAllLines($resolved, $contents, $encoding);
+}
+
+if ($Env:ConEmuBuild) {
+    Import-Module -Name oh-my-posh
+    Set-Theme Agnoster
+
+    $oldPrompt = $Function:prompt;
+
+    function prompt() {
+        & $oldPrompt;
+
+        if (Get-Command ConEmuC -ErrorAction SilentlyContinue) {
+            $gitDir = Get-GitDirectory
+            if ($gitDir) {
+                $gitRoot = Resolve-Path (Join-Path $gitDir ..)
+                $tabTitle = "Repo: $(Split-Path -Leaf $gitRoot)"
+            } else {
+                $tabTitle = Split-Path -Leaf (Get-Location)
+            }
+
+            ConEmuC -GuiMacro Rename 0 "$tabTitle" > $null 2> $null
+
+        }
+
+    }
+
+}

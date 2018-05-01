1333e7128b924d9ddef471481ccc4ad184679662
Christopher Granade <chgranad@microsoft.com>
2018-05-01 11:33:07 -0700

    Added choco profile to both Win PoSh and PoSh Core.

--- Documents/WindowsPowerShell/Microsoft.PowerShell_profile.ps1
+++ Documents/WindowsPowerShell/Microsoft.PowerShell_profile.ps1
@@ -46,3 +46,9 @@ function prompt() {
     }
 
 }
+
+# Chocolatey profile
+$ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
+if (Test-Path($ChocolateyProfile)) {
+  Import-Module "$ChocolateyProfile"
+}

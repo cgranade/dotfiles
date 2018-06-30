da148562b2dc3b1fe22d82aabb7148bb2bf77614
Christopher Granade <chgranad@microsoft.com>
2018-06-29 21:27:27 -0700

    Simplified Windows PowerShell config.

--- Documents/WindowsPowerShell/Microsoft.PowerShell_profile.ps1
+++ Documents/WindowsPowerShell/Microsoft.PowerShell_profile.ps1
@@ -1,54 +1,5 @@
-﻿# Ensure that Get-ChildItemColor is loaded
-Import-Module Get-ChildItemColor
+﻿# For the most part, we just source the PoSh Core profile.
+. ~/Documents/PowerShell/Microsoft.PowerShell_profile.ps1
 
-# Set l and ls alias to use the new Get-ChildItemColor cmdlets
-Set-Alias l Get-ChildItemColor -Option AllScope
-Set-Alias ls Get-ChildItemColorFormatWide -Option AllScope
-
-Import-Module posh-git
-Start-SshAgent -Quiet
-
+# We can now add Windows PowerShell–specific stuff.
 Add-PSSnapIn Microsoft.HPC
-
-function Remove-Bom() {
-    # See https://stackoverflow.com/a/5596984.
-    param([string] $Path);
-    $resolved = Resolve-Path $Path;
-    $contents = Get-Content $resolved;
-    $encoding = New-Object System.Text.UTF8Encoding $False;
-    [System.IO.File]::WriteAllLines($resolved, $contents, $encoding);
-}
-
-Import-Module -Name oh-my-posh
-Set-Theme Agnoster
-
-if ($Env:TERM_PROGRAM -eq "vscode") {
-    # Fix for the missing "DarkYellow" color in VS Code.
-    $ThemeSettings.Colors["GitLocalChangesColor"] = "White"
-}
-
-$oldPrompt = $Function:prompt;
-
-function prompt() {
-    & $oldPrompt;
-
-    if (Get-Command ConEmuC -ErrorAction SilentlyContinue) {
-        $gitDir = Get-GitDirectory
-        if ($gitDir) {
-            $gitRoot = Resolve-Path (Join-Path $gitDir ..)
-            $tabTitle = "Repo: $(Split-Path -Leaf $gitRoot)"
-        } else {
-            $tabTitle = Split-Path -Leaf (Get-Location)
-        }
-
-        ConEmuC -GuiMacro Rename 0 "$tabTitle" > $null 2> $null
-
-    }
-
-}
-
-# Chocolatey profile
-$ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
-if (Test-Path($ChocolateyProfile)) {
-  Import-Module "$ChocolateyProfile"
-}

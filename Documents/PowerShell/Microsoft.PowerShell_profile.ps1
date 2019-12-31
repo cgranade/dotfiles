b83a50689468e3784bdf1e6742d45cd0de8883a4
Christopher Granade <cgranade@cgranade.com>
2019-12-31 15:47:59 -0800

    Added dotnet-suggest support to $PROFILE.

--- Documents/PowerShell/Microsoft.PowerShell_profile.ps1
+++ Documents/PowerShell/Microsoft.PowerShell_profile.ps1
@@ -43,6 +43,25 @@ function prompt() {
 
 }
 
+## SUGGESTION ENGINES ########################################################
+
+# dotnet suggest shell start
+$availableToComplete = (dotnet-suggest list) | Out-String
+$availableToCompleteArray = $availableToComplete.Split([Environment]::NewLine, [System.StringSplitOptions]::RemoveEmptyEntries)
+
+
+    Register-ArgumentCompleter -Native -CommandName $availableToCompleteArray -ScriptBlock {
+        param($commandName, $wordToComplete, $cursorPosition)
+        $fullpath = (Get-Command $wordToComplete.CommandElements[0]).Source
+
+        $arguments = $wordToComplete.Extent.ToString().Replace('"', '\"')
+        dotnet-suggest get -e $fullpath --position $cursorPosition -- "$arguments" | ForEach-Object {
+            [System.Management.Automation.CompletionResult]::new($_, $_, 'ParameterValue', $_)
+        }
+    }
+$env:DOTNET_SUGGEST_SCRIPT_VERSION = "1.0.0"
+# dotnet suggest script end
+
 ## COMMANDS ##################################################################
 
 function Remove-Bom() {
